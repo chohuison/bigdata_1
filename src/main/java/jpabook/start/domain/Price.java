e51c1af5dee6b2054a75392113cadc5791d56469
eunslog <yeseo235@gmail.com>
2023-11-21 19:30:10 +0900

    part1/first

--- src/main/java/jpabook/start/domain/Price.java
+++ src/main/java/jpabook/start/domain/Price.java
@@ -1,14 +1,17 @@
 package jpabook.start.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Embeddable;
-import javax.persistence.Embedded;
 
 @Embeddable
 @Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Price {
     private int weekdayPrice;
     private int weekendPrice;
